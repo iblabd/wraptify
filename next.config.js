e00dea6d5e6bd6ed89a674c18e12d5da05ee814e
iblabd <iqbalabdurrahman04@gmail.com>
2024-01-25 15:41:31 +0700

    Add remote image config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
