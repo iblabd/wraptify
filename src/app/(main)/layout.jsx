81cbe9a52f14a9b71afe10815c642158d614b3b5
iblabd <iqbalabdurrahman04@gmail.com>
2024-01-31 14:50:29 +0700

    Add footer credit

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,10 +1,25 @@
 import Navbar from '@/components/fragments/navbar'
+import Link from 'next/link'
 
 export default function MainLayout({ children }) {
   return (
     <>
-      <Navbar />
-      <div className="container">{children}</div>
+      <div className="min-h-screen">
+        <Navbar />
+        <div className="container">{children}</div>
+      </div>
+      <div className="container mb-8 text-sm text-muted-foreground">
+        Built by{' '}
+        <Link
+          href="https://github.com/iblabd"
+          passHref
+          className="text-blue-500 underline hover:text-blue-700"
+          target="blank"
+        >
+          Iqbal Abdurrahman
+        </Link>
+        .
+      </div>
     </>
   )
 }
